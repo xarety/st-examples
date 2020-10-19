572cf3404f91c195b50722e7dcc30b19e6ae7ec0
Denis Gordeev <dgordeev@servicetitan.com>
2020-10-19 18:13:53 +0300

    rid of basename

--- packages/core/src/app.tsx
+++ packages/core/src/app.tsx
@@ -20,35 +20,35 @@ import './app.css';
 
 export const App: React.FC = () => (
     <React.StrictMode>
-        <HashBrowserRouter basename="/new/examples">
+        <HashBrowserRouter>
             <Frame>
                 <Page
                     sidebar={
                         <Sidebar>
                             <Sidebar.Section padding="y">
                                 <SideNav title="Runtime">
-                                    <SideNavLinkItem pathname="/table" exact>
+                                    <SideNavLinkItem pathname="/new/examples/table" exact>
                                         Table
                                     </SideNavLinkItem>
-                                    <SideNavLinkItem pathname="/table-master-detail">
+                                    <SideNavLinkItem pathname="/new/examples/table-master-detail">
                                         Table (Master/Detail)
                                     </SideNavLinkItem>
-                                    <SideNavLinkItem pathname="/table-state-caching">
+                                    <SideNavLinkItem pathname="/new/examples/table-state-caching">
                                         Table (State Caching)
                                     </SideNavLinkItem>
-                                    <SideNavLinkItem pathname="/confirm" exact>
+                                    <SideNavLinkItem pathname="/new/examples/confirm" exact>
                                         Confirm
                                     </SideNavLinkItem>
-                                    <SideNavLinkItem pathname="/confirm-navigation">
+                                    <SideNavLinkItem pathname="/new/examples/confirm-navigation">
                                         Confirm Navigation
                                     </SideNavLinkItem>
-                                    <SideNavLinkItem pathname="/file-uploader">
+                                    <SideNavLinkItem pathname="/new/examples/file-uploader">
                                         File Uploader
                                     </SideNavLinkItem>
-                                    <SideNavLinkItem pathname="/number-input">
+                                    <SideNavLinkItem pathname="/new/examples/number-input">
                                         Number Input
                                     </SideNavLinkItem>
-                                    <SideNavLinkItem pathname="/notifications">
+                                    <SideNavLinkItem pathname="/new/examples/notifications">
                                         Notifications
                                     </SideNavLinkItem>
                                 </SideNav>
@@ -58,15 +58,27 @@ export const App: React.FC = () => (
                     maxWidth="wide"
                 >
                     <Switch>
-                        <Route path="/table" component={TableExample} />
-                        <Route path="/table-master-detail" component={TableMasterDetailExample} />
-                        <Route path="/table-state-caching" component={TableStateCachingExample} />
-                        <Route path="/confirm" component={ConfirmExample} />
-                        <Route path="/confirm-navigation" component={ConfirmNavigationExample} />
-                        <Route path="/file-uploader" component={FileUploaderExample} />
-                        <Route path="/number-input" component={NumberInputExample} />
-                        <Route path="/notifications" component={NotificationsExample} />
-                        <Redirect path="/" to="/table" />
+                        <Route path="/new/examples/table" component={TableExample} />
+                        <Route
+                            path="/new/examples/table-master-detail"
+                            component={TableMasterDetailExample}
+                        />
+                        <Route
+                            path="/new/examples/table-state-caching"
+                            component={TableStateCachingExample}
+                        />
+                        <Route path="/new/examples/confirm" component={ConfirmExample} />
+                        <Route
+                            path="/new/examples/confirm-navigation"
+                            component={ConfirmNavigationExample}
+                        />
+                        <Route path="/new/examples/file-uploader" component={FileUploaderExample} />
+                        <Route path="/new/examples/number-input" component={NumberInputExample} />
+                        <Route
+                            path="/new/examples/notifications"
+                            component={NotificationsExample}
+                        />
+                        <Redirect path="/new/examples/" to="/new/examples/table" />
                     </Switch>
                 </Page>
             </Frame>
