29e27b54413fb76164067def94ad5f3889a19b3f
Denis Gordeev <dgordeev@servicetitan.com>
2021-02-09 18:55:19 +0300

    Configurable basename

--- packages/core/src/app.tsx
+++ packages/core/src/app.tsx
@@ -3,8 +3,10 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 import { HashBrowserRouter } from '@servicetitan/hash-browser-router';
 
+import { useOptionalDependencies } from '@servicetitan/react-ioc';
 import { SideNav, Sidebar } from '@servicetitan/design-system';
 import { SideNavLinkItem } from '@servicetitan/link-item';
+import { BASENAME_TOKEN } from '@sandboxes/web-components';
 
 import {
     TableExample,
@@ -16,68 +18,67 @@ import { BasicExample as ConfirmNavigationExample } from '@servicetitan/confirm-
 
 import { Page } from './page';
 import { ConfirmExample } from './examples/confirm';
-import { NotificationsExample } from './examples/notifications';
 
-export const App: React.FC = () => (
-    <React.StrictMode>
-        <HashBrowserRouter>
-            <Page
-                sidebar={
-                    <Sidebar>
-                        <Sidebar.Section padding="y">
-                            <SideNav title="Runtime">
-                                <SideNavLinkItem pathname="/new/examples/table" exact>
-                                    Table
-                                </SideNavLinkItem>
-                                <SideNavLinkItem pathname="/new/examples/table-master-detail">
-                                    Table (Master/Detail)
-                                </SideNavLinkItem>
-                                <SideNavLinkItem pathname="/new/examples/table-state-caching">
-                                    Table (State Caching)
-                                </SideNavLinkItem>
-                                <SideNavLinkItem pathname="/new/examples/confirm" exact>
-                                    Confirm
-                                </SideNavLinkItem>
-                                <SideNavLinkItem pathname="/new/examples/confirm-navigation">
-                                    Confirm Navigation
-                                </SideNavLinkItem>
-                                <SideNavLinkItem pathname="/new/examples/file-uploader">
-                                    File Uploader
-                                </SideNavLinkItem>
-                                <SideNavLinkItem pathname="/new/examples/number-input">
-                                    Number Input
-                                </SideNavLinkItem>
-                                <SideNavLinkItem pathname="/new/examples/notifications">
-                                    Notifications
-                                </SideNavLinkItem>
-                            </SideNav>
-                        </Sidebar.Section>
-                    </Sidebar>
-                }
-                spacing="relaxed"
-                fullHeight
-            >
-                <Switch>
-                    <Route path="/new/examples/table" component={TableExample} />
-                    <Route
-                        path="/new/examples/table-master-detail"
-                        component={TableMasterDetailExample}
-                    />
-                    <Route
-                        path="/new/examples/table-state-caching"
-                        component={TableStateCachingExample}
-                    />
-                    <Route path="/new/examples/confirm" component={ConfirmExample} />
-                    <Route
-                        path="/new/examples/confirm-navigation"
-                        component={ConfirmNavigationExample}
-                    />
-                    <Route path="/new/examples/file-uploader" component={FileUploaderExample} />
-                    <Route path="/new/examples/number-input" component={NumberInputExample} />
-                    <Route path="/new/examples/notifications" component={NotificationsExample} />
-                    <Redirect path="/new/examples/" to="/new/examples/table" />
-                </Switch>
-            </Page>
-        </HashBrowserRouter>
-    </React.StrictMode>
-);
+export const App: React.FC = () => {
+    const [basename = '/new/examples'] = useOptionalDependencies(BASENAME_TOKEN);
+
+    return (
+        <React.StrictMode>
+            <HashBrowserRouter>
+                <Page
+                    sidebar={
+                        <Sidebar>
+                            <Sidebar.Section padding="y">
+                                <SideNav title="Runtime">
+                                    <SideNavLinkItem pathname={`${basename}/table`} exact>
+                                        Table
+                                    </SideNavLinkItem>
+                                    <SideNavLinkItem pathname={`${basename}/table-master-detail`}>
+                                        Table (Master/Detail)
+                                    </SideNavLinkItem>
+                                    <SideNavLinkItem pathname={`${basename}/table-state-caching`}>
+                                        Table (State Caching)
+                                    </SideNavLinkItem>
+                                    <SideNavLinkItem pathname={`${basename}/confirm`} exact>
+                                        Confirm
+                                    </SideNavLinkItem>
+                                    <SideNavLinkItem pathname={`${basename}/confirm-navigation`}>
+                                        Confirm Navigation
+                                    </SideNavLinkItem>
+                                    <SideNavLinkItem pathname={`${basename}/file-uploader`}>
+                                        File Uploader
+                                    </SideNavLinkItem>
+                                    <SideNavLinkItem pathname={`${basename}/number-input`}>
+                                        Number Input
+                                    </SideNavLinkItem>
+                                </SideNav>
+                            </Sidebar.Section>
+                        </Sidebar>
+                    }
+                    spacing="relaxed"
+                    fullHeight
+                >
+                    <Switch>
+                        <Route path={`${basename}/table`} component={TableExample} />
+                        <Route
+                            path={`${basename}/table-master-detail`}
+                            component={TableMasterDetailExample}
+                        />
+                        <Route
+                            path={`${basename}/table-state-caching`}
+                            component={TableStateCachingExample}
+                        />
+                        <Route path={`${basename}/confirm`} component={ConfirmExample} />
+                        <Route
+                            path={`${basename}/confirm-navigation`}
+                            component={ConfirmNavigationExample}
+                        />
+                        <Route path={`${basename}/file-uploader`} component={FileUploaderExample} />
+                        <Route path={`${basename}/number-input`} component={NumberInputExample} />
+                        <Redirect path={`${basename}/`} to={`${basename}/table`} />
+                    </Switch>
+                </Page>
+            </HashBrowserRouter>
+        </React.StrictMode>
+    );
+};
